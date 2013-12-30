1f0c0b02146af2c8103835ab4027103bbe2d8407
Jorge Zamora <jz1@sanger.ac.uk>
2013-12-30 09:45:11 +0000

    added power calculations

--- script/run_deseq.R
+++ script/run_deseq.R
@@ -2,13 +2,48 @@ library(DESeq2)
 library(RColorBrewer)
 library(gplots)
 
+estimate_power<-function(design=design, 
+						 res_wald =res_wald, 
+						 max_sample_size=100){
+	
+	# the numerator of the z statistic
+	diff_vector=as.vector(res_wald$log2FoldChange)
+	
+	# to get the st. dev. I need to know the number of samples in the non reference condition level (sibling) 
+	# in R the reference level is the first level sorted alphabetically, so get the next one (index 2)
+	non_ref_sample_size = length( which ( design$condition == sort(levels(design$condition))[2]));
+	non_ref_level_name =  sort(levels(design$condition))[2];
+	sd = res_wald$lfcSE * sqrt(non_ref_sample_size )
+	
+	potential_regions<-c();
+	for(sample_size in 1:max_sample_size){
+		
+		z = diff_vector / (sd / sqrt(sample_size ))
+		pvalue = (1-pnorm(z))+pnorm(-z)
+		padj=p.adjust(pvalue,"BH")
+		potential_regions<-c(potential_regions,length(which(padj <= 0.05)))
+	}
+	power_data = data.frame(c(1:max_sample_size),potential_regions)
+	colnames(power_data)=c(paste("sample_size",non_ref_level_name,sep="__"),"potential_regions")
+	
+	
+	write.table( power_data, file=outputFilePowerMatrix, col.names=colnames(power_data), row.names=FALSE, quote=FALSE, sep="\t" )
+	pdf(file = outputPdfPowerCurve);
+	plot(power_data$sample_size,power_data$potential_regions,
+		type="l", main="z stat power curve",xlab=colnames(power_data)[1],ylab=colnames(power_data)[2])
+	dev.off();
+	
+	return(power_data);
+}
+
 Args            <- commandArgs();
 countFile       <- Args[4]
 designFile      <- Args[5]
 outputFile      <- Args[6]
 sizeFactorsFile <- Args[7]
 qcPdfFile       <- Args[8]
-outputFile_Wald      <- Args[9]
+outputFilePowerMatrix      <- Args[9]
+outputPdfPowerCurve      <- Args[10]
 
 countTable    <- read.table(  countFile , header=TRUE, row.names=1 )
 design        <- read.table( designFile, header=TRUE, row.names=1 )
@@ -118,11 +153,8 @@ res = data.frame(id=regions, pval=pval, padj=padj)
 write.table( res, file=outputFile, col.names=FALSE, row.names=FALSE,
     quote=FALSE, sep="\t" )
 
-res_wald = data.frame(id= rownames(res_wald),
-					intercept=res_wald$baseMean,
-					log2FoldChange=res_wald$log2FoldChange,
-					standard_error=res_wald$lfcSE,
-					pval=res_wald$pvalue,
-					padj=res_wald$padj)
-					
-write.table( res_wald, file=outputFile_Wald, col.names=FALSE, row.names=FALSE, quote=FALSE, sep="\t" )		
+
+
+power_data=estimate_power(design,res_wald,20);
+
+
