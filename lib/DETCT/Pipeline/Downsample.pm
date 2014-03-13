baa8a401c47d9ed177ea50b66481ac7f925d9163
Ian Sealy <ian.sealy@sanger.ac.uk>
2014-03-13 14:40:31 +0000

    Add alternative downsampling pipeline (downsamples all reads, not by tag).

--- lib/DETCT/Pipeline/Downsample.pm
+++ lib/DETCT/Pipeline/Downsample.pm
@@ -26,7 +26,9 @@ use YAML qw( DumpFile LoadFile );
 use File::Spec;
 use DETCT::Misc::BAM qw(
   stats_by_tag
+  stats_all_reads
   downsample_by_tag
+  downsample_all_reads
 );
 use DETCT::Misc::Picard qw(
   mark_duplicates
@@ -364,6 +366,262 @@ sub run_make_index {
             dir             => $job->base_filename,
             bam_file        => $bam_file,
             samtools_binary => $self->analysis->samtools_binary,
+        }
+    );
+
+    my $output_file = $job->base_filename . '.out';
+
+    DumpFile( $output_file, 1 );
+
+    return;
+}
+
+=method all_parameters_for_pre_merge
+
+  Usage       : all_parameters_for_pre_merge();
+  Purpose     : Get all parameters for pre_merge stage
+  Returns     : Array of arrayrefs
+  Parameters  : None
+  Throws      : No exceptions
+  Comments    : None
+
+=cut
+
+sub all_parameters_for_pre_merge {
+    my ($self) = @_;
+
+    my @all_parameters;
+
+    push @all_parameters, [ $self->analysis->list_all_bam_files() ];
+
+    return @all_parameters;
+}
+
+=method run_pre_merge
+
+  Usage       : run_pre_merge();
+  Purpose     : Run function for pre_merge stage
+  Returns     : undef
+  Parameters  : DETCT::Pipeline::Job
+  Throws      : No exceptions
+  Comments    : None
+
+=cut
+
+sub run_pre_merge {
+    my ( $self, $job ) = @_;
+
+    my $input_bam_files = $job->parameters;
+
+    # Merge BAM files
+    merge(
+        {
+            dir                 => $job->base_filename,
+            input_bam_files     => $input_bam_files,
+            output_bam_file     => $job->base_filename . '.bam',
+            java_binary         => $self->analysis->java_binary,
+            merge_sam_files_jar => $self->analysis->merge_sam_files_jar,
+            memory              => $job->memory,
+        }
+    );
+
+    my $output_file = $job->base_filename . '.out';
+
+    DumpFile( $output_file, 1 );
+
+    return;
+}
+
+=method all_parameters_for_stats_all_reads
+
+  Usage       : all_parameters_for_stats_all_reads();
+  Purpose     : Get all parameters for stats_all_reads stage
+  Returns     : Array of arrayrefs
+  Parameters  : None
+  Throws      : No exceptions
+  Comments    : None
+
+=cut
+
+sub all_parameters_for_stats_all_reads {
+    my ($self) = @_;
+
+    my @all_parameters;
+
+    my $bam_file =
+      File::Spec->catfile( $self->analysis_dir, 'pre_merge', '1.bam' );
+
+    push @all_parameters, [$bam_file];
+
+    return @all_parameters;
+}
+
+=method run_stats_all_reads
+
+  Usage       : run_stats_all_reads();
+  Purpose     : Run function for stats_all_reads stage
+  Returns     : undef
+  Parameters  : DETCT::Pipeline::Job
+  Throws      : No exceptions
+  Comments    : None
+
+=cut
+
+sub run_stats_all_reads {
+    my ( $self, $job ) = @_;
+
+    my ($bam_file) = @{ $job->parameters };
+
+    # Get stats for BAM file
+    my $stats = stats_all_reads(
+        {
+            bam_file => $bam_file,
+        }
+    );
+
+    my $output_file = $job->base_filename . '.out';
+
+    DumpFile( $output_file, $stats );
+
+    return;
+}
+
+=method all_parameters_for_downsample_all_reads
+
+  Usage       : all_parameters_for_downsample_all_reads();
+  Purpose     : Get all parameters for downsample_all_reads stage
+  Returns     : Array of arrayrefs
+  Parameters  : None
+  Throws      : No exceptions
+  Comments    : None
+
+=cut
+
+sub all_parameters_for_downsample_all_reads {
+    my ($self) = @_;
+
+    my @all_parameters;
+
+    my $bam_file =
+      File::Spec->catfile( $self->analysis_dir, 'pre_merge', '1.bam' );
+
+    my $stats_output_file =
+      $self->get_and_check_output_file( 'stats_all_reads', 1 );
+    my $stats      = LoadFile($stats_output_file);
+    my $read_count = $stats->{ $self->analysis->read_count_type };
+
+    push @all_parameters, [ $bam_file, $read_count ];
+
+    # If no target read count specified or larger than read count then use read
+    # count
+    if (  !$self->analysis->target_read_count
+        || $self->analysis->target_read_count > $read_count )
+    {
+        $self->analysis->set_target_read_count($read_count);
+    }
+
+    # Round down to specified whole number if required
+    if ( $self->analysis->round_down_to ) {
+        my $count = $self->analysis->target_read_count;
+        $count = $count - ( $count % $self->analysis->round_down_to );
+        $self->analysis->set_target_read_count($count);
+    }
+
+    return @all_parameters;
+}
+
+=method run_downsample_all_reads
+
+  Usage       : run_downsample_all_reads();
+  Purpose     : Run function for downsample_all_reads stage
+  Returns     : undef
+  Parameters  : DETCT::Pipeline::Job
+  Throws      : No exceptions
+  Comments    : None
+
+=cut
+
+sub run_downsample_all_reads {
+    my ( $self, $job ) = @_;
+
+    my ( $source_bam_file, $source_read_count ) = @{ $job->parameters };
+
+    # Downsample BAM file
+    my $read_count = downsample_all_reads(
+        {
+            source_bam_file   => $source_bam_file,
+            source_read_count => $source_read_count,
+            target_bam_file   => $job->base_filename . '.bam',
+            target_read_count => $self->analysis->target_read_count,
+            read_count_type   => $self->analysis->read_count_type,
+        }
+    );
+
+    # Only mark as finished if reached target read count
+    # Will happen 50% of the time
+    if ( $read_count == $self->analysis->target_read_count ) {
+        my $output_file = $job->base_filename . '.out';
+        DumpFile( $output_file, 1 );
+    }
+
+    return;
+}
+
+=method all_parameters_for_mark_duplicates_all
+
+  Usage       : all_parameters_for_mark_duplicates_all();
+  Purpose     : Get all parameters for mark_duplicates_all stage
+  Returns     : Array of arrayrefs
+  Parameters  : None
+  Throws      : No exceptions
+  Comments    : None
+
+=cut
+
+sub all_parameters_for_mark_duplicates_all {
+    my ($self) = @_;
+
+    my @all_parameters;
+
+    my $bam_file =
+      File::Spec->catfile( $self->analysis_dir, 'downsample_all_reads',
+        '1.bam' );
+
+    push @all_parameters, [$bam_file];
+
+    return @all_parameters;
+}
+
+=method run_mark_duplicates_all
+
+  Usage       : run_mark_duplicates_all();
+  Purpose     : Run function for mark_duplicates_all stage
+  Returns     : undef
+  Parameters  : DETCT::Pipeline::Job
+  Throws      : No exceptions
+  Comments    : None
+
+=cut
+
+sub run_mark_duplicates_all {
+    my ( $self, $job ) = @_;
+
+    my ($input_bam_file) = @{ $job->parameters };
+
+    my $output_bam_file = File::Spec->catfile( $self->analysis_dir,
+        $self->analysis->name . '.bam' );
+
+    # Mark duplicates
+    mark_duplicates(
+        {
+            dir                 => $job->base_filename,
+            input_bam_file      => $input_bam_file,
+            output_bam_file     => $output_bam_file,
+            metrics_file        => $job->base_filename . '.metrics',
+            java_binary         => $self->analysis->java_binary,
+            mark_duplicates_jar => $self->analysis->mark_duplicates_jar,
+            memory              => $job->memory,
+            consider_tags       => 1,
         }
     );
 
