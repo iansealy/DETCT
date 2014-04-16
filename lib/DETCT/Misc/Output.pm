bb1d2b0c20fd848b15b01fe9e2cff150a6182d06
Ian Sealy <ian.sealy@sanger.ac.uk>
2014-04-16 21:20:21 +0100

    Parse output tables (TSV and CSV) back into internal format. Will allow rerunning DESeq and/or updating gene annotation.

--- lib/DETCT/Misc/Output.pm
+++ lib/DETCT/Misc/Output.pm
@@ -23,13 +23,16 @@ use File::Slurp;
 use File::Spec;
 use File::Path qw( make_path );
 use Sort::Naturally;
+use Text::CSV;
 use List::MoreUtils qw( uniq all );
 use DETCT::Misc qw( write_or_die );
+use DETCT::Misc::R;
 
 use base qw( Exporter );
 our @EXPORT_OK = qw(
   dump_as_table
   dump_duplication_metrics
+  parse_table
 );
 
 =head1 SYNOPSIS
@@ -736,6 +739,279 @@ sub dump_html {
     write_or_die( $fh, '</tr>', "\n" );
 
     return;
+}
+
+=func parse_table
+
+  Usage       : $regions = DETCT::Misc::Output::parse_table( {
+                    analysis     => $analysis,
+                    table_file   => 'all.tsv',
+                    table_format => 'tsv',
+                } );
+  Purpose     : Parse table of regions into internal format
+  Returns     : Arrayref [
+                    Arrayref [
+                        String (region sequence name),
+                        Int (region start),
+                        Int (region end),
+                        undef,
+                        undef,
+                        String (3' end sequence name) or undef,
+                        Int (3' end position) or undef,
+                        Int (3' end strand) or undef,
+                        Int (3' end read count) or undef,
+                        Arrayref [
+                            Int (count)
+                            ...
+                        ],
+                        Arrayref [
+                            Int (normalised count)
+                            ...
+                        ],
+                        Int (p value) or undef,
+                        Int (adjusted p value) or undef,
+                        Arrayref [
+                            Int (condition fold change) or undef,
+                            Int (log2 condition fold change) or undef,
+                        ],
+                        Arrayref [
+                            Arrayref [
+                                Int (group fold change) or undef,
+                                Int (log2 group fold change) or undef,
+                            ],
+                            ... (groups)
+                        ],
+                        Hashref {
+                            String (genebuild version) => Arrayref [
+                                Arrayref [
+                                    String (gene stable id),
+                                    String (gene name) or undef,
+                                    String (gene description) or undef,
+                                    String (gene biotype),
+                                    Int (distance to 3' end),
+                                    Arrayref [
+                                        Arrayref [
+                                            String (transcript stable id),
+                                            String (transcript biotype),
+                                        ],
+                                        ... (transcripts)
+                                    ],
+                                ],
+                                ... (genes)
+                            ],
+                        }
+                    ],
+                    ... (regions)
+                ]
+  Parameters  : Hashref {
+                    analysis     => DETCT::Analysis,
+                    table_file   => String (the table file),
+                    table_format => String (the table format),
+                }
+  Throws      : If analysis is missing
+                If table file is missing
+                If table format is missing or invalid or HTML
+  Comments    : None
+
+=cut
+
+sub parse_table {
+    my ($arg_ref) = @_;
+
+    confess 'No analysis specified'     if !defined $arg_ref->{analysis};
+    confess 'No table file specified'   if !defined $arg_ref->{table_file};
+    confess 'No table format specified' if !defined $arg_ref->{table_format};
+    confess 'Cannot handle HTML format' if $arg_ref->{table_format} eq 'html';
+    if ( all { $_ ne $arg_ref->{table_format} } @FORMATS ) {
+        confess sprintf 'Invalid table format (%s) specified',
+          $arg_ref->{table_format};
+    }
+
+    # Get conditions and groups
+    my @samples = @{ $arg_ref->{analysis}->get_all_samples() };
+    my @conditions = uniq( nsort( map { $_->condition } @samples ) );
+    my @groups =
+      grep { defined $_ } uniq( nsort( map { $_->group } @samples ) );
+
+    # Get table
+    my @rows = read_file( $arg_ref->{table_file} );
+
+    # Get headings
+    my $line = shift @rows;
+    my @headings = parse_line( $line, $arg_ref->{table_format} );
+
+    # Get genebuild version from Ensembl Gene ID heading
+    ## no critic (ProhibitMagicNumbers)
+    my ($genebuild_version) = $headings[9] =~ m/\A (e\d+) \s/xms;
+    ## use critic
+
+    # Get ordinal for each count column
+    my ( %count_ordinal_for, %normalised_count_ordinal_for );
+    ## no critic (ProhibitMagicNumbers)
+    my $ordinal = 15;    # First count column
+    ## use critic
+    while ( defined $headings[$ordinal] ) {
+        if ( $headings[$ordinal] =~ m/\A ([\w.-]+) \s count \z/xms ) {
+            $count_ordinal_for{$1} = $ordinal;
+        }
+        elsif ( $headings[$ordinal] =~
+            m/\A ([\w.-]+) \s normalised \s count \z/xms )
+        {
+            $normalised_count_ordinal_for{$1} = $ordinal;
+        }
+        $ordinal++;
+    }
+
+    # Parse rows into regions
+    my @regions;
+    foreach my $row (@rows) {
+        my @row = parse_line( $row, $arg_ref->{table_format} );
+        my @region;
+        push @region, $row[0];    # Region sequence name
+        push @region, $row[1];    # Region start
+        push @region, $row[2];    # Region end
+        push @region, undef;      # Region maximum read count
+        push @region, undef;      # Region log probability sum
+        push @region, $row[0];    # 3' end sequence name
+        ## no critic (ProhibitMagicNumbers)
+        push @region, $row[3];    # 3' end position
+        push @region, $row[4];    # 3' end strand
+        push @region, $row[5];    # 3' end read count
+        ## use critic
+
+        # Counts and normalised counts, plus store for fold change calculation
+        my @counts;
+        my @normalised_counts;
+        my %counts_for_condition;
+        my %counts_for_group_condition;
+        foreach my $sample (@samples) {
+            push @counts, $row[ $count_ordinal_for{ $sample->name } ];
+            my $normalised_count =
+              $row[ $normalised_count_ordinal_for{ $sample->name } ];
+            push @normalised_counts, $normalised_count;
+            if ( scalar @conditions == 2 ) {
+                push @{ $counts_for_condition{ $sample->condition } },
+                  $normalised_count;
+            }
+            if ( scalar @conditions == 2 && scalar @groups > 1 ) {
+                push @{ $counts_for_group_condition{ $sample->group }
+                      { $sample->condition } }, $normalised_count;
+            }
+        }
+        push @region, \@counts;
+        push @region, \@normalised_counts;
+
+        ## no critic (ProhibitMagicNumbers)
+        push @region, $row[6];    # p value
+        push @region, $row[7];    # Adjusted p value
+        ## use critic
+
+        # Condition fold change
+        push @region,
+          DETCT::Misc::R::calc_condition_fold_change( \@conditions,
+            \%counts_for_condition );
+
+        # Group fold changes
+        push @region,
+          DETCT::Misc::R::calc_group_fold_changes( \@conditions, \@groups,
+            \%counts_for_group_condition );
+
+        # Gene annotation
+        # Not guaranteed to be identical to original data (e.g. no way of
+        # knowing which transcripts are associated with which genes), but data
+        # will be output correctly as a table (i.e. can roundtrip)
+        ## no critic (ProhibitMagicNumbers)
+        my @distance             = split /,/xms,   $row[8];   # 3' end distance
+        my @gene_stable_id       = split /,/xms,   $row[9];   # Gene ID
+        my @gene_biotype         = split /,/xms,   $row[10];  # Gene type
+        my @transcript_stable_id = split /,/xms,   $row[11];  # Transcript ID
+        my @transcript_biotype   = split /,/xms,   $row[12];  # Transcript type
+        my @name                 = split /,\S/xms, $row[13];  # Gene name
+        my @description          = split /,\S/xms, $row[14];  # Gene description
+        ## use critic
+        my @genes;
+
+        while (@gene_stable_id) {
+            if ( scalar @gene_stable_id == 1 ) {
+
+                # Last gene, so merge remaining data if necessary
+                @name        = ( join q{,}, @name );
+                @description = ( join q{,}, @description );
+            }
+            my @gene;
+            push @gene, shift @gene_stable_id;
+            push @gene, shift @name;
+            push @gene, shift @description;
+            push @gene, shift @gene_biotype;
+            push @gene, shift @distance;
+            if (@gene_stable_id) {
+
+                # Not last gene, so just add one transcript
+                push @gene,
+                  [ [ shift @transcript_stable_id, shift @transcript_biotype ]
+                  ];
+            }
+            else {
+                # Last gene, so add remaining transcripts
+                while (@transcript_stable_id) {
+                    push @gene,
+                      [
+                        [
+                            shift @transcript_stable_id,
+                            shift @transcript_biotype
+                        ]
+                      ];
+                }
+            }
+
+            push @genes, \@gene;
+        }
+        push @region, { $genebuild_version => \@genes };
+
+        push @regions, \@region;
+    }
+
+    return \@regions;
+}
+
+=func parse_line
+
+  Usage       : my @row = parse_line( $line, 'tsv' );
+  Purpose     : Parse line of TSV or CSV into fields
+  Returns     : Array
+  Parameters  : String (the line from a table)
+                String (the table format)
+  Throws      : If CSV can't be parsed
+  Comments    : None
+
+=cut
+
+sub parse_line {
+    my ( $line, $format ) = @_;
+
+    $line =~ s/[\r\n]+ \z//xms;
+
+    my @row;
+
+    if ( $format eq 'tsv' ) {
+        @row = split /\t/xms, $line;
+        @row = map { $_ eq q{-} || length $_ == 0 ? undef : $_ } @row;
+    }
+    elsif ( $format eq 'csv' ) {
+        confess "Can't parse CSV: $line" if !_csv()->parse($line);
+        @row = _csv()->fields();
+    }
+
+    return @row;
+}
+
+# Cache Text::CSV object
+{
+    my $text_csv_object = Text::CSV->new( { empty_is_undef => 1 } );
+
+    sub _csv {
+        return $text_csv_object;
+    }
 }
 
 =func dump_duplication_metrics
