16988a6fb595b69e7fec8edde40d5d5074583610
Ian Sealy <ian.sealy@sanger.ac.uk>
2015-06-11 18:15:14 +0100

    Plot proportion of variance explained by each region across chromosome.

--- script/pca.R
+++ script/pca.R
@@ -3,6 +3,7 @@
 suppressPackageStartupMessages(library(DESeq2))
 suppressPackageStartupMessages(library(genefilter))
 suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(reshape2))
 
 Args               <- commandArgs()
 dataFile           <- ifelse(is.na(Args[6]),  "all.tsv",     Args[6])
@@ -45,13 +46,13 @@ if (transformMethod == "rlog") {
 rv <- rowVars(assay(dds))
 select <- order(rv, decreasing=TRUE)[seq_len(min(regionCount, length(rv)))]
 pca <- prcomp(t(assay(dds)[select,]))
-percentVarPC <- pca$sdev^2 / sum( pca$sdev^2 )
+propVarPC <- pca$sdev^2 / sum( pca$sdev^2 )
 aload <- abs(pca$rotation)
-percentVarRegion <- sweep(aload, 2, colSums(aload), "/")
+propVarRegion <- sweep(aload, 2, colSums(aload), "/")
 
 # Output regions contributing most to each PC
-for (i in seq.int(sum(percentVarPC * 100 >= varPCThreshold))) {
-    data[select, "% variance explained"] <- percentVarRegion[,i] * 100
+for (i in seq.int(sum(propVarPC * 100 >= varPCThreshold))) {
+    data[select, "% variance explained"] <- propVarRegion[,i] * 100
     topData <- subset(data[order(data$`% variance explained`,
         decreasing=TRUE),], `% variance explained` >= 0.1)
     if (grepl("csv$", dataFile)) {
@@ -66,26 +67,45 @@ for (i in seq.int(sum(percentVarPC * 100 >= varPCThreshold))) {
 # Write PCs to PDF
 pdf(pdfFile)
 
-d <- data.frame(PC=seq.int(length(percentVarPC)), var=percentVarPC)
+# Show variance explained for each PC
+d <- data.frame(PC=seq.int(length(propVarPC)), var=propVarPC)
 print(ggplot(data=d, aes(x=PC, y=var)) + geom_line() + geom_point() +
     xlab("PC") + ylab("Variance explained") + ylim(c(0, 1)))
 print(ggplot(data=d, aes(x=PC, y=cumsum(var))) + geom_line() + geom_point() +
     xlab("PC") + ylab("Cumulative variance explained") + ylim(c(0, 1)))
 
+# Plot PCs in pairs
 intgroup.df <- as.data.frame(colData(dds)[, c("condition"), drop=FALSE])
 group <- factor(apply( intgroup.df, 1, paste, collapse=" : "))
-
-for (i in seq.int(sum(percentVarPC * 100 >= varPCThreshold) - 1)) {
+for (i in seq.int(sum(propVarPC * 100 >= varPCThreshold) - 1)) {
     first <- i
     second <- i + 1
     d <- data.frame(first=pca$x[,first], second=pca$x[,second], group=group,
         intgroup.df, name=colnames(dds))
     print(ggplot(data=d, aes_string(x="first", y="second", color="group"))
         + geom_point(size=3) + 
-        xlab(paste0("PC", first, ": ", round(percentVarPC[first] * 100, 1),
+        xlab(paste0("PC", first, ": ", round(propVarPC[first] * 100, 1),
             "% variance")) +
-        ylab(paste0("PC", second, ": ", round(percentVarPC[second] * 100, 1),
+        ylab(paste0("PC", second, ": ", round(propVarPC[second] * 100, 1),
             "% variance")))
+}
+
+# Plot proportion of variance explained by each region across chromosome
+lastSigPC <- sum(propVarPC * 100 >= varPCThreshold)
+varMax <- max(propVarRegion[,1:lastSigPC])
+propVarRegion <- as.data.frame(propVarRegion[,1:lastSigPC])
+propVarRegion$region <- rowMeans(data[select, c(2,3)])
+propVarRegion$chr <- data[select, 1]
+lastChr <- max(as.numeric(levels(data[,1])[grepl("^[0-9]+$",
+    levels(data[,1]))]))
+for (chr in seq.int(lastChr)) {
+    var_long <- melt(propVarRegion[propVarRegion$chr == chr,1:ncol(propVarRegion)-1],
+        id="region", variable.name="PC")
+    print(ggplot(data=var_long, aes(x=region, y=value, colour=PC)) +
+        geom_line() + geom_point() +
+        xlab(paste0("Chromosome ", chr)) +
+        ylab("Variance explained") +
+        ylim(c(0, varMax)))
 }
 
 graphics.off()
