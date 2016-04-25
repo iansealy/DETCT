5ac01605a54131f23d1239eaa3e66fce51a6487a
Ian Sealy <git@iansealy.com>
2016-04-25 10:40:42 +0100

    Script to convert DETCT output to BioLayout Express3D format.

---
+++ script/convert_to_biolayout.pl
@@ -0,0 +1,214 @@
+#!/usr/bin/env perl
+
+# PODNAME: convert_to_biolayout.pl
+# ABSTRACT: Convert DETCT output into BioLayout Express3D format
+
+## Author         : is1
+## Maintainer     : is1
+## Created        : 2016-04-22
+## Last commit by : $Author$
+## Last modified  : $Date$
+## Revision       : $Revision$
+## Repository URL : $HeadURL$
+
+use warnings;
+use strict;
+use autodie;
+use Carp;
+use Try::Tiny;
+
+use Getopt::Long;
+use Pod::Usage;
+use Readonly;
+use DETCT::Misc qw( printf_or_die );
+use DETCT::Misc::Output;
+
+=head1 DESCRIPTION
+
+
+=head1 EXAMPLES
+
+
+=cut
+
+# Constants
+Readonly our @FIELDS_BEFORE_LFC => ( 0 .. 8 );
+Readonly our @FIELDS_AFTER_LFC  => ( 9 .. 14 );
+
+# Default options
+my $input_file;
+my $samples_file;
+my ( $help, $man );
+
+# Get and check command line options
+get_and_check_options();
+
+my ( $sample_cols, $lfc_cols ) = output_header( $input_file, $samples_file );
+output_regions( $input_file, $sample_cols, $lfc_cols );
+
+# Get regions of interest
+sub output_header {
+    my ( $input_file, $samples_file ) = @_;   ## no critic (ProhibitReusedNames)
+
+    my ($extension) = $input_file =~ m/[.] ([[:lower:]]{3}) \z/xms;
+    if ( !$extension || ( $extension ne 'csv' && $extension ne 'tsv' ) ) {
+        confess sprintf '%s is not .csv or .tsv file', $input_file;
+    }
+
+    open my $input_fh, '<', $input_file;
+    my $header = <$input_fh>;
+    $header =~ s/\A [#]//xms;
+    my @headings = DETCT::Misc::Output::parse_line( $header, $extension );
+    close $input_fh;
+
+    my @sample_cols;
+    my @lfc_cols;
+    my $i = -1;    ## no critic (ProhibitMagicNumbers)
+    foreach my $heading (@headings) {
+        $i++;
+        if ( $heading =~ m/normalised \s count \z/xms ) {
+            push @sample_cols, $i;
+        }
+        if ( $heading =~ m/\A Log2 \s fold \s change/xms ) {
+            push @lfc_cols, $i;
+        }
+    }
+
+    my @output_headings = ('ID');
+    push @output_headings, @headings[@FIELDS_BEFORE_LFC];
+    push @output_headings, @headings[@lfc_cols];
+    push @output_headings, @headings[@FIELDS_AFTER_LFC];
+    push @output_headings, @headings[@sample_cols];
+
+    my @tmp_headings;
+    foreach my $heading (@output_headings) {
+        $heading =~ s/\s normalised \s count \z//xms;
+        push @tmp_headings, $heading;
+    }
+    @output_headings = @tmp_headings;
+    @output_headings = map { /\s/xms ? qq{"$_"} : $_ } @output_headings;
+
+    printf_or_die( "%s\n", join "\t", @output_headings );
+
+    return \@sample_cols, \@lfc_cols if !$samples_file;
+
+    open my $samples_fh, '<', $samples_file;
+    $header = <$samples_fh>;
+    $header =~ s/\A \s+//xms;
+    my @columns = split /\s+/xms, $header;
+    close $samples_fh;
+
+    my $cols_before_samples =
+      scalar @FIELDS_BEFORE_LFC + scalar @lfc_cols + scalar @FIELDS_AFTER_LFC;
+    foreach my $col ( 1 .. scalar @columns ) {
+        @headings = ( $columns[ $col - 1 ], (q{}) x $cols_before_samples );
+        open my $samples_fh, '<', $samples_file;
+        $header = <$samples_fh>;
+        while ( my $line = <$samples_fh> ) {
+            chomp $line;
+            my @fields = split /\s+/xms, $line;
+            push @headings, $fields[$col];
+        }
+        close $samples_fh;
+        @headings = map { /\s/xms ? qq{"$_"} : $_ } @headings;
+        printf_or_die( "%s\n", join "\t", @headings );
+    }
+
+    return \@sample_cols, \@lfc_cols;
+}
+
+# Output regions of interest
+sub output_regions {
+    ## no critic (ProhibitReusedNames)
+    my ( $file, $sample_cols, $lfc_cols ) = @_;
+    ## use critic
+
+    my ($extension) = $file =~ m/[.] ([[:lower:]]{3}) \z/xms;
+    if ( !$extension || ( $extension ne 'csv' && $extension ne 'tsv' ) ) {
+        confess sprintf '%s is not .csv or .tsv file', $file;
+    }
+
+    open my $fh, '<', $file;    ## no critic (RequireBriefOpen)
+    my $header = <$fh>;
+    while ( my $line = <$fh> ) {
+        my @fields = DETCT::Misc::Output::parse_line( $line, $extension );
+
+        # Get ID by joining gene name, chr, start, end, 3' end position & strand
+        ## no critic (ProhibitMagicNumbers)
+        my $id = join q{:}, @fields[ 13, 0, 1, 2, 4 ];
+        ## use critic
+
+        my @output_fields = ($id);
+        push @output_fields, @fields[@FIELDS_BEFORE_LFC];
+        push @output_fields, @fields[ @{$lfc_cols} ];
+        push @output_fields, @fields[@FIELDS_AFTER_LFC];
+        push @output_fields, @fields[ @{$sample_cols} ];
+
+        @output_fields = map { defined $_ ? $_       : q{-} } @output_fields;
+        @output_fields = map { /\s/xms    ? qq{"$_"} : $_ } @output_fields;
+
+        printf_or_die( "%s\n", join "\t", @output_fields );
+    }
+    close $fh;
+
+    return;
+}
+
+# Get and check command line options
+sub get_and_check_options {
+
+    # Get options
+    GetOptions(
+        'input_file=s'   => \$input_file,
+        'samples_file=s' => \$samples_file,
+        'help'           => \$help,
+        'man'            => \$man,
+    ) or pod2usage(2);
+
+    # Documentation
+    if ($help) {
+        pod2usage(1);
+    }
+    elsif ($man) {
+        pod2usage( -verbose => 2 );
+    }
+
+    # Check options
+    if ( !$input_file ) {
+        pod2usage("--input_file must be specified\n");
+    }
+
+    return;
+}
+
+=head1 USAGE
+
+    convert_to_biolayout.pl
+        [--input_file file]
+        [--samples_file file]
+        [--help]
+        [--man]
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--input_file FILE>
+
+Differential expression pipeline output file (e.g. all.tsv).
+
+=item B<--samples_file FILE>
+
+Differential expression pipeline DESeq2 samples file (e.g. samples.txt).
+
+=item B<--help>
+
+Print a brief help message and exit.
+
+=item B<--man>
+
+Print this script's manual page and exit.
+
+=back
+
+=cut
