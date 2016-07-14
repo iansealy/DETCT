95e004516b18c819dca833a72cb10851f410b422
Ian Sealy <git@iansealy.com>
2016-07-14 16:08:34 +0100

    Object for finding transposon nearest to a position.

---
+++ lib/DETCT/TransposonFinder.pm
@@ -0,0 +1,170 @@
+## no critic (RequireUseStrict, RequireUseWarnings, RequireTidyCode)
+package DETCT::TransposonFinder;
+## VERSION
+## use critic
+
+# ABSTRACT: Object for finding transposons by location
+
+## Author         : is1
+## Maintainer     : is1
+## Created        : 2016-07-14
+## Last commit by : $Author$
+## Last modified  : $Date$
+## Revision       : $Revision$
+## Repository URL : $HeadURL$
+
+use warnings;
+use strict;
+use autodie;
+use Carp;
+use Try::Tiny;
+
+use Class::InsideOut qw( private register id );
+use Number::Closest::XS qw( find_closest_numbers );
+
+=head1 SYNOPSIS
+
+    # Brief code examples
+
+=cut
+
+# Attributes:
+private slice_adaptor => my %slice_adaptor;  # Bio::EnsEMBL::DBSQL::SliceAdaptor
+private cache         => my %cache;          # hashref
+
+=method new
+
+  Usage       : my $transposon_finder = DETCT::TransposonFinder->new( {
+                    slice_adaptor => $slice_adaptor,
+                } );
+  Purpose     : Constructor for transposon finder objects
+  Returns     : DETCT::TransposonFinder
+  Parameters  : Hashref {
+                    slice_adaptor    => Bio::EnsEMBL::DBSQL::SliceAdaptor,
+                }
+  Throws      : No exceptions
+  Comments    : None
+
+=cut
+
+sub new {
+    my ( $class, $arg_ref ) = @_;
+    my $self = register($class);
+    $self->set_slice_adaptor( $arg_ref->{slice_adaptor} );
+    return $self;
+}
+
+=method slice_adaptor
+
+  Usage       : my $slice_adaptor = $transposon_finder->slice_adaptor;
+  Purpose     : Getter for Ensembl slice adaptor attribute
+  Returns     : Bio::EnsEMBL::DBSQL::SliceAdaptor
+  Parameters  : None
+  Throws      : No exceptions
+  Comments    : None
+
+=cut
+
+sub slice_adaptor {
+    my ($self) = @_;
+    return $slice_adaptor{ id $self};
+}
+
+=method set_slice_adaptor
+
+  Usage       : $transposon_finder->set_slice_adaptor($slice_adaptor);
+  Purpose     : Setter for Ensembl slice adaptor attribute
+  Returns     : undef
+  Parameters  : Bio::EnsEMBL::DBSQL::SliceAdaptor
+  Throws      : No exceptions
+  Comments    : None
+
+=cut
+
+sub set_slice_adaptor {
+    my ( $self, $arg ) = @_;
+    $slice_adaptor{ id $self} = _check_slice_adaptor($arg);
+    return;
+}
+
+# Usage       : $slice_adaptor = _check_slice_adaptor($slice_adaptor);
+# Purpose     : Check for valid Ensembl slice adaptor
+# Returns     : Bio::EnsEMBL::DBSQL::SliceAdaptor
+# Parameters  : Bio::EnsEMBL::DBSQL::SliceAdaptor
+# Throws      : If slice adaptor is missing or invalid (i.e. not a
+#               Bio::EnsEMBL::DBSQL::SliceAdaptor object)
+# Comments    : None
+sub _check_slice_adaptor {
+    my ($slice_adaptor) = @_;
+    return $slice_adaptor
+      if defined $slice_adaptor
+      && $slice_adaptor->isa('Bio::EnsEMBL::DBSQL::SliceAdaptor');
+    confess 'No Ensembl slice adaptor specified' if !defined $slice_adaptor;
+    confess 'Class of Ensembl slice adaptor (', ref $slice_adaptor,
+      ') not Bio::EnsEMBL::DBSQL::SliceAdaptor';
+}
+
+=method get_nearest_transposon
+
+  Usage       : $finder->get_nearest_transposon($seq_name, $pos, $strand);
+  Purpose     : Retrieve the nearest transposon to a 3' end
+  Returns     : Int (distance)
+                Int (nearest transposon position)
+  Parameters  : String (the 3' end sequence name)
+                Int (the 3' end position)
+                Int (the 3' end strand)
+  Throws      : No exceptions
+  Comments    : Distance is positive if downstream of 3' end and negative if
+                upstream
+
+=cut
+
+sub get_nearest_transposon {
+    my ( $self, $seq_name, $pos, $strand ) = @_;
+
+    # Ensure cache is filled
+    $self->_fill_cache_from_ensembl($seq_name);
+
+    my $transposon_positions = $cache{ id $self}->{$seq_name};
+
+    my $distance;
+    my $nearest_transposon_pos;
+
+    my $nearest = find_closest_numbers( $pos, $transposon_positions );
+    if ( @{$nearest} ) {
+        $nearest_transposon_pos = $nearest->[0];
+    }
+    $distance = $pos - $nearest_transposon_pos;
+    if ( $strand > 0 ) {
+        $distance *= -1;
+    }
+
+    return $distance, $nearest_transposon_pos;
+}
+
+sub _fill_cache_from_ensembl {
+    my ( $self, $seq_name ) = @_;
+
+    # Skip if cache already filled
+    return if exists $cache{ id $self}->{$seq_name};
+
+    # Make sure default key exists (in case there are no transposons)
+    $cache{ id $self}->{$seq_name} = [];
+
+    my $slice = $self->slice_adaptor->fetch_by_region( 'toplevel', $seq_name );
+    return if !defined $slice;    # No transposons if non-existent sequence name
+
+    my %ends;
+    my $repeat_features = $slice->get_all_RepeatFeatures();
+    foreach my $repeat_feature ( @{$repeat_features} ) {
+        next
+          if $repeat_feature->repeat_consensus->repeat_type !~ m/Transposon/xms;
+        $ends{ $repeat_feature->seq_region_start } = 1;
+        $ends{ $repeat_feature->seq_region_end }   = 1;
+    }
+    $cache{ id $self}->{$seq_name} = [ sort { $a <=> $b } keys %ends ];
+
+    return;
+}
+
+1;
---
+++ t/transposonfinder.t
@@ -0,0 +1,85 @@
+use warnings;
+use strict;
+use autodie;
+use Test::More;
+use Test::Exception;
+use Test::Warn;
+use Test::DatabaseRow;
+use Test::MockObject;
+use Carp;
+
+plan tests => 16;
+
+use DETCT::TransposonFinder;
+
+# Mock repeat features
+my @repeat_features;
+my @repeat_parameters =
+  ( [ 10, 20, 'Transposon' ], [ 30, 40, 'Not' ], [ 50, 60, 'Transposon' ] );
+foreach my $repeat_parameters (@repeat_parameters) {
+    my ( $start, $end, $type ) = @{$repeat_parameters};
+
+    # Create reepat consensus
+    my $consensus = Test::MockObject->new();
+    $consensus->set_always( 'repeat_type', $type );
+
+    # Create repeat feature
+    my $feature = Test::MockObject->new();
+    $feature->set_always( 'seq_region_start', $start );
+    $feature->set_always( 'seq_region_end',   $end );
+    $feature->set_always( 'repeat_consensus', $consensus );
+    push @repeat_features, $feature;
+}
+
+# Mock slice
+my $slice = Test::MockObject->new();
+$slice->set_always( 'get_all_RepeatFeatures', \@repeat_features );
+
+# Mock slice adaptor
+my $slice_adaptor = Test::MockObject->new();
+$slice_adaptor->set_isa('Bio::EnsEMBL::DBSQL::SliceAdaptor');
+$slice_adaptor->set_always( 'fetch_by_region', $slice );
+
+my $transposon_finder =
+  DETCT::TransposonFinder->new( { slice_adaptor => $slice_adaptor, } );
+
+isa_ok( $transposon_finder, 'DETCT::TransposonFinder' );
+
+# Test Ensembl slice adaptor attribute
+isa_ok( $transposon_finder->slice_adaptor,
+    'Bio::EnsEMBL::DBSQL::SliceAdaptor' );
+throws_ok { $transposon_finder->set_slice_adaptor() }
+qr/No Ensembl slice adaptor specified/ms, 'No Ensembl slice adaptor';
+throws_ok { $transposon_finder->set_slice_adaptor('invalid') }
+qr/Class of Ensembl slice adaptor/ms, 'Invalid Ensembl slice adaptor';
+
+my $distance;
+my $nearest_transposon_pos;
+
+# Nearest to single transposon
+( $distance, $nearest_transposon_pos ) =
+  $transposon_finder->get_nearest_transposon( '1', 1, 1 );
+is( $distance,               9,  q{Transposon is 9 bp downstream} );
+is( $nearest_transposon_pos, 10, q{Transposon starts at 10 bp} );
+( $distance, $nearest_transposon_pos ) =
+  $transposon_finder->get_nearest_transposon( '1', 1, -1 );
+is( $distance,               -9, q{Transposon is 9 bp upstream} );
+is( $nearest_transposon_pos, 10, q{Transposon starts at 10 bp} );
+
+# Nearest to non-transposon repeat
+( $distance, $nearest_transposon_pos ) =
+  $transposon_finder->get_nearest_transposon( '1', 29, 1 );
+is( $distance,               -9, q{Transposon is 9 bp upstream} );
+is( $nearest_transposon_pos, 20, q{Transposon ends at 20 bp} );
+( $distance, $nearest_transposon_pos ) =
+  $transposon_finder->get_nearest_transposon( '1', 29, -1 );
+is( $distance,               9,  q{Transposon is 9 bp downstream} );
+is( $nearest_transposon_pos, 20, q{Transposon ends at 20 bp} );
+( $distance, $nearest_transposon_pos ) =
+  $transposon_finder->get_nearest_transposon( '1', 41, 1 );
+is( $distance,               9,  q{Transposon is 9 bp downstream} );
+is( $nearest_transposon_pos, 50, q{Transposon starts at 50 bp} );
+( $distance, $nearest_transposon_pos ) =
+  $transposon_finder->get_nearest_transposon( '1', 41, -1 );
+is( $distance,               -9, q{Transposon is 9 bp upstream} );
+is( $nearest_transposon_pos, 50, q{Transposon starts at 50 bp} );
