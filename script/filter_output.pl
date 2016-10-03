782731723c1b3903f51b1503c3db9437192afb22
Ian Sealy <git@iansealy.com>
2016-10-03 13:04:47 +0100

    Allow filtering by end in CDS, by biotype and by proximity to annotated 3' end.

--- script/filter_output.pl
+++ script/filter_output.pl
@@ -22,6 +22,9 @@ use Pod::Usage;
 use Readonly;
 use Path::Tiny;
 use File::Spec;
+use Sort::Naturally;
+use Set::IntervalTree;
+use DETCT::GeneFinder;
 use DETCT::Analysis::DiffExpr;
 use DETCT::Misc::Output;
 
@@ -30,6 +33,13 @@ use DETCT::Misc::Output;
 
 =head1 EXAMPLES
 
+    perl \
+        -Ibranch-ensembl-74/ensembl/modules \
+        filter_output.pl \
+            --dir filter \
+            --analysis_yaml analysis.yaml \
+            --all_file all.tsv \
+            --ends_file ends.tsv
 
 =cut
 
@@ -46,15 +56,21 @@ Readonly our $TRANSPOSON_POSITION_FIELD           => 8;
 Readonly our $CONTINUOUS_RNASEQ_TRANSCRIPTS_FIELD => 9;
 
 Readonly our $DISTANCE_TO_POLY_A => 10;
+Readonly our $DISTANCE_THRESHOLD => 100;
 
-Readonly our %IS_PRIMARY_HEXAMER => map { $_ => 1 } qw( AATAAA ATTAAA );
+Readonly our %IS_PRIMARY_HEXAMER   => map { $_ => 1 } qw( AATAAA ATTAAA );
+Readonly our %IS_SECONDARY_HEXAMER => map { $_ => 1 }
+  qw( AGTAAA TATAAA CATAAA GATAAA AATATA AATACA AATAGA ACTAAA AAGAAA AATGAA );
 
 # Default options
 my $analysis_dir = q{.};
 my $analysis_yaml = File::Spec->catfile( $analysis_dir, 'analysis.yaml' );
 my $all_file;
 my $ends_file;
+my $slice_regexp;
+my @biotype;
 my $keep_regions_without_ends;
+my $keep_ends_in_cds;
 my $keep_polya_ends;
 my $keep_ends_without_hexamer;
 my $keep_ends_without_rnaseq;
@@ -66,25 +82,47 @@ get_and_check_options();
 # Create analysis
 my $analysis = DETCT::Analysis::DiffExpr->new_from_yaml($analysis_yaml);
 
-my $ends_for = get_ends($ends_file);
+my $gene_finder = DETCT::GeneFinder->new(
+    {
+        slice_adaptor     => $analysis->slice_adaptor,
+        skip_transcripts  => $analysis->get_all_skip_transcripts,
+        ensembl_db_types  => $analysis->get_all_ensembl_db_types,
+        required_biotypes => \@biotype,
+    }
+);
 
-my $regions = get_regions( $all_file, $analysis );
+my $cds_cache = get_cds_cache( $analysis->slice_adaptor, $slice_regexp );
+
+my $ends_for = get_ends( $ends_file, $slice_regexp );
+
+my $regions = get_regions( $all_file, $analysis, $slice_regexp );
+
+if (@biotype) {
+    $regions = remove_ends_by_biotype( $regions, $gene_finder );
+}
+
+if ( !$keep_ends_in_cds ) {
+    $regions = remove_ends_in_cds( $regions, $cds_cache );
+}
 
 if ( !$keep_polya_ends ) {
     $regions = remove_polya( $regions, $ends_for );
 }
 
 if ( !$keep_ends_without_hexamer ) {
-    $regions = remove_ends_without_hexamer( $regions, $ends_for );
+    $regions = remove_ends_without_hexamer( $regions, $ends_for, $gene_finder );
 }
 
 if ( !$keep_ends_without_rnaseq ) {
-    $regions = remove_ends_without_rnaseq( $regions, $ends_for );
+    $regions = remove_ends_without_rnaseq( $regions, $ends_for, $gene_finder );
 }
 
 if ( !$keep_regions_without_ends ) {
     $regions = remove_regions_without_ends($regions);
 }
+
+# Reannotate (perhaps only with required biotypes)
+$regions = $gene_finder->add_gene_annotation($regions);
 
 DETCT::Misc::Output::dump_as_table(
     {
@@ -94,8 +132,54 @@ DETCT::Misc::Output::dump_as_table(
     }
 );
 
+sub get_cds_cache {
+    my ( $sa, $slice_regexp ) = @_;    ## no critic (ProhibitReusedNames)
+
+    my %cds_cache;
+
+    # Get all transcripts
+    my $slices = $sa->fetch_all('toplevel');
+    if ($slice_regexp) {
+        @{$slices} =
+          grep { $_->seq_region_name =~ m/\A $slice_regexp \z/xms } @{$slices};
+    }
+    foreach
+      my $slice ( sort { ncmp( $a->seq_region_name, $b->seq_region_name ) }
+        @{$slices} )
+    {
+        my $chr = $slice->seq_region_name;
+        if ( !exists $cds_cache{$chr} ) {
+            $cds_cache{$chr}{-1} = Set::IntervalTree->new;
+            $cds_cache{$chr}{1}  = Set::IntervalTree->new;
+        }
+
+        my $transcripts = $slice->get_all_Transcripts();
+        foreach my $transcript ( @{$transcripts} ) {
+            if ( defined $transcript->translation ) {
+                my $strand = $transcript->seq_region_strand;
+                my $exons  = $transcript->get_all_Exons();
+                foreach my $exon ( @{$exons} ) {
+                    next if !defined $exon->coding_region_start($transcript);
+                    $cds_cache{$chr}{$strand}->insert(
+                        {
+                            start => $exon->coding_region_start($transcript),
+                            end   => $exon->coding_region_end($transcript),
+                        },
+                        $exon->coding_region_start($transcript),
+                        $exon->coding_region_end($transcript) + 1
+                    );
+                }
+            }
+        }
+    }
+
+    return \%cds_cache;
+}
+
 sub get_ends {
-    my ($file) = @_;
+    ## no critic (ProhibitReusedNames)
+    my ( $file, $slice_regexp ) = @_;
+    ## use critic
 
     my ($extension) = $file =~ m/[.] ([[:lower:]]{3}) \z/xms;
     if ( !$extension || ( $extension ne 'csv' && $extension ne 'tsv' ) ) {
@@ -108,6 +192,7 @@ sub get_ends {
     my $header = <$fh>;
     while ( my $line = <$fh> ) {
         my @fields = DETCT::Misc::Output::parse_line( $line, $extension );
+        next if $slice_regexp && $fields[0] !~ m/\A $slice_regexp \z/xms;
 
         # Get region ID from chr, start or end (depending on strand) and strand
         ## no critic (ProhibitMagicNumbers)
@@ -125,20 +210,86 @@ sub get_ends {
 }
 
 sub get_regions {
-    my ( $file, $analysis ) = @_;    ## no critic (ProhibitReusedNames)
+    ## no critic (ProhibitReusedNames)
+    my ( $file, $analysis, $slice_regexp ) = @_;
+    ## use critic
 
     my ($extension) = $file =~ m/[.] ([[:lower:]]{3}) \z/xms;
     if ( !$extension || ( $extension ne 'csv' && $extension ne 'tsv' ) ) {
         confess sprintf '%s is not .csv or .tsv file', $file;
     }
 
-    return DETCT::Misc::Output::parse_table(
+    ## no critic (ProhibitReusedNames)
+    my $regions = DETCT::Misc::Output::parse_table(
+        ## use critic
         {
             analysis     => $analysis,
             table_file   => $file,
             table_format => $extension,
         }
     );
+
+    return $regions if !$slice_regexp;
+
+    my @new_regions;
+    foreach my $region ( @{$regions} ) {
+        next if $region->[0] !~ m/\A $slice_regexp \z/xms;
+        push @new_regions, $region;
+    }
+
+    return \@new_regions;
+}
+
+sub remove_ends_by_biotype {
+    my ( $regions, $gene_finder ) = @_;    ## no critic (ProhibitReusedNames)
+
+    foreach my $region ( @{$regions} ) {
+        my $seq_name = $region->[0];
+        ## no critic (ProhibitMagicNumbers)
+        my $three_prime_end_pos = $region->[6];
+        my $strand              = $region->[7];
+        ## use critic
+        next if !defined $three_prime_end_pos;
+        my @three_prime_end_pos =
+          ref $three_prime_end_pos eq 'ARRAY'
+          ? @{$three_prime_end_pos}
+          : ($three_prime_end_pos);
+        foreach my $pos (@three_prime_end_pos) {
+            my ( undef, $distance ) =
+              $gene_finder->get_nearest_transcripts( $seq_name, $pos, $strand );
+            if ( !defined $distance ) {
+                $region = remove_end_from_region( $region, $pos );
+            }
+        }
+    }
+
+    return $regions;
+}
+
+sub remove_ends_in_cds {
+    my ( $regions, $cds_cache ) = @_;    ## no critic (ProhibitReusedNames)
+
+    foreach my $region ( @{$regions} ) {
+        my $chr = $region->[0];
+        ## no critic (ProhibitMagicNumbers)
+        my $strand              = $region->[7];
+        my $three_prime_end_pos = $region->[6];
+        ## use critic
+        next if !defined $three_prime_end_pos;
+        my @three_prime_end_pos =
+          ref $three_prime_end_pos eq 'ARRAY'
+          ? @{$three_prime_end_pos}
+          : ($three_prime_end_pos);
+        foreach my $pos (@three_prime_end_pos) {
+            my $cds_intervals =
+              $cds_cache->{$chr}{$strand}->fetch( $pos, $pos + 1 );
+            if ( @{$cds_intervals} ) {
+                $region = remove_end_from_region( $region, $pos );
+            }
+        }
+    }
+
+    return $regions;
 }
 
 sub remove_polya {
@@ -171,12 +322,16 @@ sub remove_polya {
 }
 
 sub remove_ends_without_hexamer {
-    my ( $regions, $ends_for ) = @_;    ## no critic (ProhibitReusedNames)
+    ## no critic (ProhibitReusedNames)
+    my ( $regions, $ends_for, $gene_finder ) = @_;
+    ## use critic
 
     foreach my $region ( @{$regions} ) {
         my @all_ends = parse_ends( $region, $ends_for );
+        my $seq_name = $region->[0];
         ## no critic (ProhibitMagicNumbers)
         my $three_prime_end_pos = $region->[6];
+        my $strand              = $region->[7];
         ## use critic
         next if !defined $three_prime_end_pos;
         my @three_prime_end_pos =
@@ -186,9 +341,26 @@ sub remove_ends_without_hexamer {
         foreach my $pos (@three_prime_end_pos) {
             my ($end) =
               grep { $_->[$THREE_PRIME_END_POS_FIELD] == $pos } @all_ends;
-            if (   !defined $end->[$HEXAMER_FIELD]
-                || !exists $IS_PRIMARY_HEXAMER{ $end->[$HEXAMER_FIELD] } )
+
+            # Keep end if primary hexamer
+            next
+              if defined $end->[$HEXAMER_FIELD]
+              && exists $IS_PRIMARY_HEXAMER{ $end->[$HEXAMER_FIELD] };
+
+            # Or keep end if secondary hexamer and near annotated 3' end
+            my $remove = 1;
+            if ( defined $end->[$HEXAMER_FIELD]
+                && exists $IS_SECONDARY_HEXAMER{ $end->[$HEXAMER_FIELD] } )
             {
+                my ( undef, $distance ) =
+                  $gene_finder->get_nearest_transcripts( $seq_name, $pos,
+                    $strand );
+                if ( defined $distance && abs $distance <= $DISTANCE_THRESHOLD )
+                {
+                    $remove = 0;
+                }
+            }
+            if ($remove) {
                 $region = remove_end_from_region( $region, $pos );
             }
         }
@@ -198,12 +370,16 @@ sub remove_ends_without_hexamer {
 }
 
 sub remove_ends_without_rnaseq {
-    my ( $regions, $ends_for ) = @_;    ## no critic (ProhibitReusedNames)
+    ## no critic (ProhibitReusedNames)
+    my ( $regions, $ends_for, $gene_finder ) = @_;
+    ## use critic
 
     foreach my $region ( @{$regions} ) {
         my @all_ends = parse_ends( $region, $ends_for );
+        my $seq_name = $region->[0];
         ## no critic (ProhibitMagicNumbers)
         my $three_prime_end_pos = $region->[6];
+        my $strand              = $region->[7];
         ## use critic
         next if !defined $three_prime_end_pos;
         my @three_prime_end_pos =
@@ -213,7 +389,14 @@ sub remove_ends_without_rnaseq {
         foreach my $pos (@three_prime_end_pos) {
             my ($end) =
               grep { $_->[$THREE_PRIME_END_POS_FIELD] == $pos } @all_ends;
-            if ( !$end->[$CONTINUOUS_RNASEQ_TRANSCRIPTS_FIELD] ) {
+
+            # Keep end if continuous RNA-Seq
+            next if $end->[$CONTINUOUS_RNASEQ_TRANSCRIPTS_FIELD];
+
+            # Or keep end if near annotated 3' end
+            my ( undef, $distance ) =
+              $gene_finder->get_nearest_transcripts( $seq_name, $pos, $strand );
+            if ( !defined $distance || abs $distance > $DISTANCE_THRESHOLD ) {
                 $region = remove_end_from_region( $region, $pos );
             }
         }
@@ -341,7 +524,10 @@ sub get_and_check_options {
         'analysis_yaml=s'           => \$analysis_yaml,
         'all_file=s'                => \$all_file,
         'ends_file=s'               => \$ends_file,
+        'slice_regexp=s'            => \$slice_regexp,
+        'biotype=s@{,}'             => \@biotype,
         'keep_regions_without_ends' => \$keep_regions_without_ends,
+        'keep_ends_in_cds'          => \$keep_ends_in_cds,
         'keep_polya_ends'           => \$keep_polya_ends,
         'keep_ends_without_hexamer' => \$keep_ends_without_hexamer,
         'keep_ends_without_rnaseq'  => \$keep_ends_without_rnaseq,
@@ -375,7 +561,10 @@ sub get_and_check_options {
         [--analysis_yaml file]
         [--all_file file]
         [--ends_file file]
+        [--slice_regexp regexp]
+        [--biotype biotype...]
         [--keep_regions_without_ends]
+        [--keep_ends_in_cds]
         [--keep_polya_ends]
         [--keep_ends_without_hexamer]
         [--keep_ends_without_rnaseq]
@@ -402,9 +591,21 @@ DETCT output file (containing all regions).
 
 DETCT 3' ends file.
 
+=item B<--slice_regexp REGEXP>
+
+Regular expression for matching slice names.
+
+=item B<--biotype BIOTYPES>
+
+Required biotype(s).
+
 =item B<--keep_regions_without_ends>
 
 Don't filter out regions lacking a 3' end.
+
+=item B<--keep_ends_in_cds>
+
+Don't filter 3' ends in a transcript's CDS.
 
 =item B<--keep_polya_ends>
 
